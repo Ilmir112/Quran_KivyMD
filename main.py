a2f0b1ec3b9c45cd04b4ec52363638509b2457d6
ilmir112 <Zufaroff112@gmail.com>
2023-08-11 13:25:58 +0500

    поменял buildoze, скорректировал загрузку mp3

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.properties import ListProperty
 from kivy.uix.button import Button
 from kivy.core.audio import SoundLoader
 from kivymd.uix.menu import MDDropdownMenu
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 from kivy.core.window import Window
 from kivymd.uix.snackbar import Snackbar
 from kivymd.uix.gridlayout import GridLayout
